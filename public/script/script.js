6fbc22ab3aa337a53a8c7a7752a59defb7578cba
KimInteger <kjs20151240@gmail.com>
2024-06-05 20:44:09 +0900

    흰건반에 click이벤트를 통한 press효과를 주었다.

--- public/script/script.js
+++ public/script/script.js
@@ -3,3 +3,16 @@ const keyboard = document.getElementsByTagName('button');
 const blackKey = document.getElementsByClassName('black');
 
 const whiteKey = document.getElementsByClassName('white');
+
+function changeC(e) {
+  e.target.style.backgroundColor = 'rgb(232,232,232)';
+  setTimeout(()=>{
+    e.target.style.backgroundColor = 'rgb(255,255,255)';
+  },100);
+};
+
+console.log(whiteKey);
+
+for(let i = 0; i < whiteKey.length; i++){
+  whiteKey[i].addEventListener('click',changeC);
+};
