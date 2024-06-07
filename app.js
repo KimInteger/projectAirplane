de37e44d915a97228b208b1b5aedcde306231d73
KimInteger <kjs20151240@gmail.com>
2024-06-07 19:52:08 +0900

    서버 파일 작성중 fileUtills제작

--- app.js
+++ app.js
@@ -2,6 +2,46 @@ const http = require('node:http');
 
 const fs = require('node:fs');
 
-const qs = require('node:querystring');
-
 const path = require('node:path');
+
+const mimeType = {
+  '.html' : 'text/html; charset=UTF-8',
+  '.css' : 'text/css; charset=UTF-8',
+  '.js' : 'application/javascript; charset=UTF-8',
+  '.json' : 'application/json; charset=UTF-8',
+  '.ico' : 'img/x-icon'
+};
+
+const fileUtills = {
+  getFilepath : function(url){
+    let filePath = '';
+    if(url === '/'){
+      filePath = './public.index.html';
+    } else if(url.endsWith('.js')) {
+      filePath = `./public/script${url}`;
+    } else if(url.endsWith('css')) {
+      filePath = `./public/CSS${url}`;
+    } else {
+      return;
+    }
+    return filePath;
+  },
+  getExtention : function(filePath){
+    let ext = path.extname(filePath);
+    return ext.toLowerCase();
+  },
+  getContentType : function(ext){
+    let contentType = '';
+    if(mimeType.hasOwnProperty(ext)){
+      contentType = mimeType[ext];
+    } else {
+      contentType = 'text/plain; charset=UTF-8';
+    }
+    return contentType;
+  }
+};
+
+function notFound(res){
+  res.writeHead(404,{"Content-Type":"text/plain;charset=UTF-8"});
+  res.end('페이지를 찾을 수 없습니다.');
+}
