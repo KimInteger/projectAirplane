bf4c547c9ba5485e3067656959f6e92af7034b99
KimInteger <kjs20151240@gmail.com>
2024-06-07 17:09:36 +0900

    함수 모듈화

--- public/script/audioModule.js
+++ public/script/audioModule.js
@@ -1,4 +1,13 @@
-const audioCtx = new AudioContext;
-
-let osc = audioCtx.createOscillator();
-
+export function addOsc(hertz) {
+  const audioCtx = new AudioContext;
+  
+  
+  let osc = audioCtx.createOscillator();
+  osc.type = "square";
+  osc.frequency.setValueAtTime(hertz, audioCtx.currentTime); 
+  osc.connect(audioCtx.destination);
+  osc.start();
+  setTimeout(()=>{
+    osc.stop();
+  },500);
+}
